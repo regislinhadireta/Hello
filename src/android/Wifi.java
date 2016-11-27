8df3fed9a2a516344482db45c5eb26b639699a56
regislinhadireta <regislinhadireta@gmail.com>
2016-11-26 22:12:17 -0200

    Update Wifi.java

--- src/android/Wifi.java
+++ src/android/Wifi.java
@@ -9,7 +9,7 @@ public class Wifi extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
 
-        if (action.equals("greet")) {
+        if (action.equals("getSSID")) {
 
             String name = data.getString(0);
             String message = "Trabalhando, " + name;
