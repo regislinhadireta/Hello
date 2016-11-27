a826324a3ffcdd0e821653adfe6efc0e8411e8bf
regislinhadireta <regislinhadireta@gmail.com>
2016-11-26 22:43:10 -0200

    Update wifi.js

--- www/wifi.js
+++ www/wifi.js
@@ -1,13 +1,5 @@
-/*global cordova, module*/
-
-var Wifi {
-    getSSID: function(win, fail) {
-        if (typeof win != "function") {
-            console.log("getSSID informe win e fail para continuar.");
-            return;
-        }
+module.exports = {
+    getSSID: function (successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "Wifi", "getSSID", [""]);
     }
 };
-
-module.exports = Wifi;
